ce1fb6ebcc94ff896a8284344d69b7d9b9034996
Vladyslav Riabyi <vriabyi@e2r5p11.unit.ua>
2019-10-31 23:03:46 +0200

    mem

---
+++ libmx/src/mx_memcpy.c
@@ -0,0 +1,20 @@
+#include "libmx.h"
+
+void *mx_memcpy(void *restrict dst, const void *restrict src, size_t n) {
+	
+	char *dt = dst;
+	const char *sc = src;
+
+	while(n--)
+		*dt++ = *sc++;
+
+	return dst;
+}
+/*
+int main() {
+    void *restrict b = malloc(5);
+	const void *restrict src = malloc(5);
+	src = "0123";
+    mx_memcpy(b, src, 5);
+    printf("%s", b);
+}*/
--- libmx/src/mx_memset.c
+++ libmx/src/mx_memset.c
@@ -1,4 +1,17 @@
 #include "libmx.h"
 
 void *mx_memset(void *b, int c, size_t len) {
+
+	unsigned char *s = b;
+
+	while(len--) {
+		*s++ = c;
+	}
+	return b;
 }
+/*
+int main() {
+	void *b = malloc(5);
+	mx_memset(b, '4', 5);
+	printf("%s", b);
+} */

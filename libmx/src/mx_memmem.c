3a88f9223f7a811ce6aca87945ab56c918a66450
Vladyslav Riabyi <vriabyi@e2r5p11.unit.ua>
2019-11-01 21:19:52 +0200

    sdfasdf

---
+++ libmx/src/mx_memmem.c
@@ -0,0 +1,22 @@
+#include "libmx.h"
+#include <string.h>
+
+void *mx_memmem(const void *big, size_t big_len, const void *little, size_t little_len) {
+
+	const char *bg = big;
+	const char *lt = little;
+
+	if (big_len < little_len && big_len == 0 && little_len == 0) return NULL;
+
+	if (mx_strstr(bg, lt)) return mx_strstr(bg, lt);
+
+	return NULL;
+}
+/*
+int main() {
+    void *s = "0123456789";
+	void *s1 = "23";
+
+    printf("%s\n", mx_memmem(s, 10, s1, 1));
+    printf("%s\n", memmem(s, 10, s1, 1));
+} */	
---
+++ libmx/src/mx_memrchr.c
@@ -0,0 +1,22 @@
+#include "libmx.h"
+
+void *mx_memrchr(const void *s, int c, size_t n) {
+	unsigned char c1 = c;
+    unsigned const char *s1 = s;
+
+	while(*s1)
+		s1++;
+	s1--;
+    while(*s1 && n--) {
+        if (*s1 == c1)
+            return (char*)s1;
+            s1--;
+        }
+    return NULL;
+}
+/*
+int main() {
+
+    void *s = "0123456789";
+    printf("%s\n", mx_memrchr(s, '4', 3));
+} */
